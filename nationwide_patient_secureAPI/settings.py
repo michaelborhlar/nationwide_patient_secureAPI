034ebb590f01c2e77966cc90c4c041ad88f08a7a
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-25 22:13:49 +0100

    added some files

--- nationwide_patient_secureAPI/settings.py
+++ nationwide_patient_secureAPI/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY", default="fallback-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
