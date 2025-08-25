ef55eb4b230e358e139364c0d5f32dabc8ed782c
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-25 22:23:10 +0100

    corrected some errors

--- nationwide_patient_secureAPI/settings.py
+++ nationwide_patient_secureAPI/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['nationwide-patient-secureapi.onrender.com',  'localhost']
 
 
 # Application definition
