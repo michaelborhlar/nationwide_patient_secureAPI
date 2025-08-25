16785c49637e4027c7795bebfc2a9c9e6b891273
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-25 17:43:55 +0100

    my first test deploy

--- nationwide_patient_secureAPI/settings.py
+++ nationwide_patient_secureAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*h=u@t2pamcjobo$okk$@(9hqkmueu+pgk^mp4qiojoo#!svpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
