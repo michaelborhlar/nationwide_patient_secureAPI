d64e0f1d98cf347ddb165749d600d8572afd8c24
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-04 15:39:16 +0100

    de

--- nationwide_patient_secureAPI/settings.py
+++ nationwide_patient_secureAPI/settings.py
@@ -137,6 +137,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
