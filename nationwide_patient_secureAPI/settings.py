39e8dccec010b98d275195b0a03a7c5221d72b59
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-09 15:29:12 +0100

    corrections

--- nationwide_patient_secureAPI/settings.py
+++ nationwide_patient_secureAPI/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,11 +43,8 @@ INSTALLED_APPS = [
     "corsheaders",
     'rest_framework',
     'nationwide_patient_secure_api',
-    'cloudinary',
-    'cloudinary_storage',
 ]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 MIDDLEWARE = [
